3e321286026ef8ac264e6b2a56ea26de4b571784
MikhailAksenov1 <165162036+MikhailAksenov1@users.noreply.github.com>
2024-05-31 13:15:13 +0300

    Update 1.py

--- Home work 11/1.py
+++ Home work 11/1.py
@@ -2,5 +2,9 @@ import calendar, locale
 from datetime import date
 for i in range(1, 13):
     locale.setlocale(locale.LC_ALL, 'ru')
-    mydate = date(2024, i, calendar.monthcalendar(2024, i)[2][3])
+    if calendar.monthcalendar(2024, i)[2][3] < 15:
+        thu = calendar.monthcalendar(2024, i)[3][3]
+    else:
+        thu = calendar.monthcalendar(2024, i)[2][3]
+    mydate = date(2024, i, thu)
     print(mydate.strftime('%A %d %B %Y'))
